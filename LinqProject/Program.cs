697f78c87ba07997f188f1504477e7565a3e88bf
RobertJMitchell <rjrocks32@gmail.com>
2015-03-19 10:28:37 -0500

    starting base
    starting linq source code

---
+++ LinqProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nameArr = {"Sue", "Kelly", "John Footpenis", "RJ", "Corinne"}; // array of strings for LINQ IENumerable example
+            IEnumerable<string> results = nameArr.Where(n => n.Length >= 3); 
+            foreach (string str in results)
+            {
+                Console.WriteLine(str);
+            }
+            Console.ReadLine();
+        }
+    }
+}
