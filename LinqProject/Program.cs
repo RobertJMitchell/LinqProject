cf909804e60fb0f5b101d5ab23c8a803e815583f
RobertJMitchell <rjrocks32@gmail.com>
2015-03-19 10:40:58 -0500

    updated new string containing lambda exp to search by char

--- LinqProject/Program.cs
+++ LinqProject/Program.cs
@@ -10,9 +10,20 @@ namespace LinqProject
     {
         static void Main(string[] args)
         {
-            string[] nameArr = {"Sue", "Kelly", "John Footpenis", "RJ", "Corinne"}; // array of strings for LINQ IENumerable example
+            //dataset...
+            string[] nameArr = {"Sue", "Mancy", "Michelle", "John Footpenis", "RJ", "Corinne"}; // array of strings for LINQ IENumerable example
+            //LINQ - Length of Characters Greater/Equal to 3
             IEnumerable<string> results = nameArr.Where(n => n.Length >= 3); 
-            foreach (string str in results)
+            //LINQ - Check if name contains XYZ
+            IEnumerable<string> results2 = nameArr
+                .Where(n => n.Contains("M"))
+                .OrderBy(n => n.Length)
+                .Select(n => n.ToUpper());
+            //a collection...
+
+            //loop through strings
+            
+            foreach (string str in results2)
             {
                 Console.WriteLine(str);
             }
