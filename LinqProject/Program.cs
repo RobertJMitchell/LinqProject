5e8a9c28e66403bbd3b6f9050a214c8f82e07d73
RobertJMitchell <rjrocks32@gmail.com>
2015-03-19 11:31:53 -0500

    added constructor for ToDo

--- LinqProject/Program.cs
+++ LinqProject/Program.cs
@@ -29,13 +29,37 @@ namespace LinqProject
             {
                 Console.WriteLine(str);
             }
+
+            //List of Todos
+            List<Todo> tList = new List<Todo>
+            {
+                new Todo("Rick", "Burn Couch", false, DateTime.Now.AddDays(-10), DateTime.Now.AddDays(-5) );
+            };
+
+
             Console.ReadLine();
+
         }
     }
 
     // Todo Class
     public class Todo 
     {
+        private string p1;
+        private string p2;
+        private bool p3;
+        private DateTime dateTime1;
+        private DateTime dateTime2;
+
+        public Todo(string p1, string p2, bool p3, DateTime dateTime1, DateTime dateTime2)
+        {
+            // TODO: Complete member initialization
+            this.p1 = p1;
+            this.p2 = p2;
+            this.p3 = p3;
+            this.dateTime1 = dateTime1;
+            this.dateTime2 = dateTime2;
+        }
         //User - Person that Todo is Assigned to
         public string User { get; set; }
         // Name - Name of the Task to be completed
